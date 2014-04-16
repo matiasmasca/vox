1ba5dd02fae6f1846fe7b543fd41a61f865d122a
Matías Mascazzini <matiasmasca@gmail.com>
2014-04-16 01:09:41 -0300

    Key para travis

--- config/application.rb
+++ config/application.rb
@@ -19,9 +19,14 @@ module Vox
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+	# config/application.rb
+	# TODO Remove this in Rails 4.1
+	config.secret_key_base = YAML.load(File.open("#{Rails.root}/config/secrets.yml"))[Rails.env]['secret_key_base']
     
   end
 end
+
 
 #Agragando Bootstrap a los mensajes de los formularios
 =begin
