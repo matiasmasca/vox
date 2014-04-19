4144885a20c449388e4ac16c8e7ea8965ed11351
Matías Mascazzini <matiasmasca@gmail.com>
2014-04-19 19:42:50 -0300

    Remplazado migrate por db:setup, para evitar los migrate distintos

--- db/migrate/20140305193052_change_column_duration_to_selection_process.rb
+++ db/migrate/20140305193052_change_column_duration_to_selection_process.rb
@@ -2,17 +2,17 @@ class ChangeColumnDurationToSelectionProcess < ActiveRecord::Migration
   def up
     change_table :selection_process do |t|
     	#Para SQLite
-      #change_column :selection_process, :duration, :integer
+      change_column :selection_process, :duration, :integer
       #Para Postgres
-      change_column :selection_process, :duration, 'integer USING CAST(duration AS integer)'
+      #change_column :selection_process, :duration, 'integer USING CAST(duration AS integer)'
     end
   end
  
   def down
     change_table :selection_process do |t|
-    	#change_column :selection_process, :duration, :string
+    	change_column :selection_process, :duration, :string
       #Para Postgres
-      change_column :selection_process, :duration, 'string USING CAST(duration AS string)'
+      #change_column :selection_process, :duration, 'string USING CAST(duration AS string)'
     end
   end
 end
