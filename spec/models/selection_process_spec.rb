c396adeb010b72882d87843c9f3f5205ca3cd2e2
Matías Mascazzini <matiasmasca@gmail.com>
2014-06-05 19:23:35 -0300

    migraciones locas

--- spec/models/selection_process_spec.rb
+++ spec/models/selection_process_spec.rb
@@ -10,6 +10,19 @@ describe SelectionProcess do
     #@to-do
     it "Probar el controlador! las validaciones" do
     	pending "Falta probar las validaciones del modelo! este fue el primero que se hizo con scafolding"
+    #longitud de datos
+    #validates :name_process, length: { in: 8..250, message: "debe ser entre 8 y 250 caracteres" }
+    #validates :place, length: { in: 8..250, message: "debe ser entre 8 y 250 caracteres" }, :allow_blank => true
+
+    #validates :duration, inclusion: { in: 1..365, message: "%{value} debe ser entre 1 y 365" }, :allow_blank => true, numericality: { only_integer: true }
+    
+    #datos unicos.
+    #validates :name_process, uniqueness: { message: "ya está siendo utilizado.", :case_sensitive => false }
+    end
+
+    it "es invalido, nombre debe ser mayor que 7" do
+        name_process = SelectionProcess.new({ "name_process" => "MyStrin" , "place" => "MyString", "organizer_id" => 1})
+        name_process.should_not be_valid
     end
 
     it "debe estar asociado a una Organización" do
